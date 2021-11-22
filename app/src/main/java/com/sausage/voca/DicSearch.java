986c5bb18afb2294f935036143918daa4e1033b8
leesiha <mlice1030@soongsil.ac.kr>
2021-11-22 18:15:38 +0900

    갱신

--- app/src/main/java/com/sausage/voca/DicSearch.java
+++ app/src/main/java/com/sausage/voca/DicSearch.java
@@ -3,13 +3,35 @@ package com.sausage.voca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DicSearch extends AppCompatActivity {
 
     private EditText search_result; //이게 회색이면 안 쓰인거니까 뭐가 문제인지 눈여겨볼것...
+    private TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +39,157 @@ public class DicSearch extends AppCompatActivity {
 
         search_result = findViewById(R.id.search_result);
         //나 대체 왜 그랬는지는 모르겠는데 search_result.findViewById(R.id.search_result); 라고 자꾸 써서 error냈다... 정신차려
+        result = findViewById(R.id.result);
 
         Intent intent = getIntent();
         CharSequence search = intent.getExtras().getCharSequence("search");
         search_result.setText(search);
+
+        search_result.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    RunThread();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
+
+    public void RunThread() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String str = main(search_result.getText().toString());
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            result.setText(str);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public static String main(String args) throws JSONException {
+        String clientId = "9nlapeOCtCRa3WR16aMM";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "ddORBhc9I9";//애플리케이션 클라이언트 시크릿값";
+
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        try {
+            text = URLEncoder.encode(args, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        String responseBody = post(apiURL, requestHeaders, text);
+
+        try {
+            JSONObject jsonObject = new JSONObject(responseBody);
+            String message = jsonObject.getString("message");
+            JSONObject subJsonObject = new JSONObject(message);
+            String result = jsonObject.getString("result");
+            JSONObject subJsonObject2 = new JSONObject(result);
+            result = jsonObject.getString("translatedText");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void jsonParsing(String json)
+    {
+        try{
+            JSONObject jsonObject = new JSONObject(json);
+
+            JSONArray movieArray = jsonObject.getJSONArray("Movies");
+
+            for(int i=0; i<movieArray.length(); i++)
+            {
+                JSONObject movieObject = movieArray.getJSONObject(i);
+
+                Movie movie = new Movie();
+
+                movie.setTitle(movieObject.getString("title"));
+                movie.setGrade(movieObject.getString("grade"));
+                movie.setCategory(movieObject.getString("category"));
+
+                movieList.add(movie);
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text) {
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=en&target=ko&text=" + text; //원본언어: 목적언어: 영어 (en) -> 한국어 (ko)
+        try {
+            con.setRequestMethod("POST");
+            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl) {
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection) url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body) {
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+
 }
